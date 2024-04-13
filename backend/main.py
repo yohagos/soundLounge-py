8c4f15b01be4e9dd297af1579c869bbd17402c11
yohagos <yosef.hagos@googlemail.com>
2024-04-13 19:04:23 +0200

    fix: adding routes and implementing a init_db function to load test data

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from routes import users
+
+from db.database import engine, SessionLocal
+from db.init_db import create_init_users
+from models.models import Base
+
 app = FastAPI()
 
 app.add_middleware(
@@ -11,5 +17,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(users.router)
 
+Base.metadata.create_all(engine)
 
+def init_db():
+    db = SessionLocal()
+    create_init_users(db)
+    db.close()
+
+init_db()
