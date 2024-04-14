300ad9f2f83784feb545e8da799257527dab1935
yohagos <yosef.hagos@googlemail.com>
2024-04-14 15:42:46 +0200

    fix: user routes, adding get test data

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -5,7 +5,8 @@ from typing import List
 
 from db.database import get_db
 from schemas.users import UserFull
-from controller.user_services import get_user_list
+from schemas.followers import Followers
+from controller.user_services import get_user_list, get_followers_for_test_user_1
 
 router = APIRouter(
     prefix='/users',
@@ -15,3 +16,7 @@ router = APIRouter(
 @router.get('', response_model=List[UserFull])
 def get_users(db: Session = Depends(get_db)):
     return get_user_list(db)
+
+@router.get('followers', response_model=List[Followers])
+def get_followers(db: Session = Depends(get_db)):
+    return get_followers_for_test_user_1(db)
