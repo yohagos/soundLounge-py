eded4bfde46804e7afc3c252d0f4cf82bb2ebae5
yohagos <yosef.hagos@googlemail.com>
2024-04-14 15:42:19 +0200

    fix: adding config to base user model

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from datetime import datetime
 
 class UserBase(BaseModel):
     firstname: str
@@ -7,10 +8,24 @@ class UserBase(BaseModel):
     username: str
     password: str
 
+class User(BaseModel):
+    firstname: str
+    lastname: str
+    class Config:
+        orm_mode = True
+
 class UserFull(BaseModel):
     id: int
     firstname: str
     lastname: str
     username: str
     password: str
-    created_at: str
+    created_at: datetime
+    
+    #followed_by: List[User]
+    following: List[User]
+    class Config:
+        orm_mode = True
+
+#UserFull.update_forward_refs()
+
