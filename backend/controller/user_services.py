d538563eef504e9fb16c146490c14652407557a1
yohagos <yosef.hagos@googlemail.com>
2024-04-14 15:41:53 +0200

    fix: testing follower table

--- backend/controller/user_services.py
+++ backend/controller/user_services.py
@@ -3,10 +3,12 @@ from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 
 from schemas.users import UserBase
-from models.models import User as UserModel
+from models.models import User as UserModel, Follower as FollowerModel
 
-#def get_user_list():
 
 def get_user_list(db: Session):
     return db.query(UserModel).all()
 
+def get_followers_for_test_user_1(db: Session):
+    user = db.query(UserModel).filter(UserModel.username == 'yosef@yosef.de').first()
+    return db.query(FollowerModel).filter(FollowerModel.user_id == user.id).all()
