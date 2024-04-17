7a26cb966eab86ffbd61e4ddb2936cd1bbd673ac
yohagos <yosef.hagos@googlemail.com>
2024-04-17 14:55:02 +0200

    feat: remove/delete user

--- backend/controller/user.py
+++ backend/controller/user.py
@@ -2,10 +2,13 @@ from fastapi import status, HTTPException
 from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 from datetime import datetime
+import logging
 
 from auth.hashing import get_password_hashed
 from schemas.users import UserBase
 from models.models import User as UserModel, Follower as FollowerModel
+
+logging.basicConfig(level=logging.INFO)
 
 
 def get_user_list(db: Session):
@@ -26,7 +29,26 @@ def create_user(request: UserBase, db: Session):
     db.add(new_user)
     db.commit()
 
+def delete_user(username: str, db: Session):
+    try:
+        user = db.query(UserModel).filter(UserModel.username == username).first()
+        if user:
+            # Lösche die Einträge in der Follower-Tabelle, die den Benutzer als follower_id haben
+            db.query(UserModel).filter(FollowerModel.follower_id == user.id).delete()
+            # Lösche die Einträge in der Follower-Tabelle, die den Benutzer als user_id haben
+            db.query(FollowerModel).filter(FollowerModel.user_id == user.id).delete()
+            # Lösche den Benutzer selbst
+            db.delete(user)
+            db.commit()
+            logging.info(f"Benutzer {username} und seine Follower wurden erfolgreich gelöscht.")
+        else:
+            logging.warning(f"Benutzer {username} wurde nicht gefunden.")
+    except Exception as e:
+        logging.error(f"Fehler beim Löschen des Benutzers {username}: {str(e)}")
+        db.rollback()
 
-def get_followers_for_test_user_1(db: Session):
-    user = db.query(UserModel).filter(UserModel.username == 'yosef@yosef.de').first()
-    return db.query(FollowerModel).filter(FollowerModel.user_id == user.id).all()
+def get_follower_for_user(db: Session, user_id: int):
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if user:
+        return user.following
+    return None
--- backend/routes/users.py
+++ backend/routes/users.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from db.database import get_db
-from schemas.users import UserFull, UserBase
+from schemas.users import UserFull, UserBase, User
 from schemas.followers import Followers
-from controller.user import get_user_list, create_user, get_followers_for_test_user_1
+from controller.user import get_user_list, create_user, delete_user, get_follower_for_user
 
 router = APIRouter(
     prefix='/users',
@@ -15,17 +15,28 @@ router = APIRouter(
 
 @router.get('', response_model=List[UserFull])
 def get_users(db: Session = Depends(get_db)):
+    """ 
+    Get a list of all Users
+    """
     return get_user_list(db)
 
 @router.post('')
 def create_new_user(request: UserBase, db: Session = Depends(get_db)):
     """ 
-    Create a new user 
+    Create a new User 
     - username will be the email adress
     - password  is hashed before saving to database, min 8 length, atleast 1 uppercase, atleast 1 digit and 1 special character
     """
     return create_user(request, db)
 
-@router.get('/followers', response_model=List[Followers])
-def get_followers(db: Session = Depends(get_db)):
-    return get_followers_for_test_user_1(db)
+@router.delete('')
+async def remove_user(username: str, db: Session = Depends(get_db)):
+    delete_user(username, db)
+    return 'Done'
+
+@router.get('/{user_id}/followers', response_model=List[User])
+def get_followers(user_id: int, db: Session = Depends(get_db)):
+    """ 
+    Get Followers for specific User 
+    """
+    return get_follower_for_user(db, user_id)
